970189613ec52b5ee96379365c0670abfc3a3f5c
truecube <truecube@gmail.com>
2022-02-02 22:14:35 -0500

    updating client-id for prod

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
-let client_id = 76724;
-let client_secret = '68bbb0b199fdfa96891031d0b91a6fb2f3973adc';
+// let client_id = 76724;
+// let client_secret = '68bbb0b199fdfa96891031d0b91a6fb2f3973adc';
 
-// let client_id = 73172;
-// let client_secret = 'e61fba5596fa4b374d77cfd71d501fcb2f7b583e';
+let client_id = 73172;
+let client_secret = 'e61fba5596fa4b374d77cfd71d501fcb2f7b583e';
