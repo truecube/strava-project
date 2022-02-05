43867e39a0407fd145e62674174f8efdeb03615e
truecube <truecube@gmail.com>
2022-02-05 18:22:37 -0500

    extracting expiryDays into settings page

--- settings.js
+++ settings.js
@@ -1,5 +1,21 @@
-// let client_id = 76724;
-// let client_secret = '68bbb0b199fdfa96891031d0b91a6fb2f3973adc';
+//Adhithya's
+const client_id = 76724;
+const client_secret = '68bbb0b199fdfa96891031d0b91a6fb2f3973adc';
 
-let client_id = 73172;
-let client_secret = 'e61fba5596fa4b374d77cfd71d501fcb2f7b583e';
+//TheBestDad's
+// const client_id = 73172;
+// const client_secret = 'e61fba5596fa4b374d77cfd71d501fcb2f7b583e';
+
+//other global variables
+const refresh_token_expiry_in_days=30
+const access_token_expiry_in_days=1
+
+const environment = "dev" // "stage", "beta", "prod"
+
+function consoleLog(message) {
+    if(environment == "prod") {
+        //not going to log it - since it is production
+    } else {
+        console.log(message)
+    }
+}
